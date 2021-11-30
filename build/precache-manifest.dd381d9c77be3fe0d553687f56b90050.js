d4ca4853fd630eb8f136bd98d64f4dfcf8414c5b
LeoHarada <86167421+LeoHarada@users.noreply.github.com>
2021-11-30 01:50:39 -0800

    Build Folder

---
+++ build/precache-manifest.dd381d9c77be3fe0d553687f56b90050.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2844640d4e3d2d882bb53368d643123e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fddcb5f938d20359c83d",
+    "url": "/static/css/main.2cd8e94f.chunk.css"
+  },
+  {
+    "revision": "8c385b501ac9575d1078",
+    "url": "/static/js/2.262093c3.chunk.js"
+  },
+  {
+    "revision": "fddcb5f938d20359c83d",
+    "url": "/static/js/main.0d9fcc1b.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
